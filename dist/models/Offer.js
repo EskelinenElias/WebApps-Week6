b5fc5ee78b01cdab53a9d7c1506f68db85bd6468
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-12-09 18:26:51 +0200

    Task 1: added all compiled code

---
+++ dist/models/Offer.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Offer = void 0;
+const mongoose_1 = require("mongoose");
+const OfferSchema = new mongoose_1.Schema({
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: false }
+});
+const Offer = (0, mongoose_1.model)("Todo", OfferSchema);
+exports.Offer = Offer;
+exports.default = Offer;
